e24e9a0916a1c622699fcf91669ef3c1d193d640
leventeharman <21d_harmanl@nyirszikszi.hu>
2026-03-15 20:41:20 +0100

    refactor: felesleges felhatalmazottak() metodus eltavolitasa
    - A Spring Security automatikusan kezeli a ROLE_ prefixet
    - A metodus sosem volt meghivva, torlese tisztitja a kodot

--- sulieszkoz_kolcsonzo/src/main/java/com/kolcsonzo/suli/sulieszkoz_kolcsonzo/enums/FelhasznaloSzerepkor.java
+++ sulieszkoz_kolcsonzo/src/main/java/com/kolcsonzo/suli/sulieszkoz_kolcsonzo/enums/FelhasznaloSzerepkor.java
@@ -1,9 +1,5 @@
 package com.kolcsonzo.suli.sulieszkoz_kolcsonzo.enums;
 
 public enum FelhasznaloSzerepkor {
-    FELHASZNALO, ADMIN, ALKALMAZOTT;
-
-    public String felhatalmazottak(){
-        return "ROLE_" + name();
-    }
+    FELHASZNALO, ADMIN, ALKALMAZOTT
 }
