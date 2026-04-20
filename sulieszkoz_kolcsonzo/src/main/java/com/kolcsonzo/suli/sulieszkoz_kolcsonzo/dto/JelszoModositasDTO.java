b99de9482a05c3f7041d454933fa63b178b0a27b
leventehrmn <21d_harmanl@nyirszikszi.hu>
2026-04-20 15:02:01 +0200

    fix: Jelszó és szerepkör módosítási végpontok hozzáadása, konstruktorok hozzáadása

--- sulieszkoz_kolcsonzo/src/main/java/com/kolcsonzo/suli/sulieszkoz_kolcsonzo/dto/JelszoModositasDTO.java
+++ sulieszkoz_kolcsonzo/src/main/java/com/kolcsonzo/suli/sulieszkoz_kolcsonzo/dto/JelszoModositasDTO.java
@@ -1,9 +1,13 @@
 package com.kolcsonzo.suli.sulieszkoz_kolcsonzo.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class JelszoModositasDTO {
     @NotBlank(message = "A régi jelszó megadása kötelező")
     private String regiJelszo;
--- sulieszkoz_kolcsonzo/src/main/java/com/kolcsonzo/suli/sulieszkoz_kolcsonzo/dto/SzerepkorModositasDTO.java
+++ sulieszkoz_kolcsonzo/src/main/java/com/kolcsonzo/suli/sulieszkoz_kolcsonzo/dto/SzerepkorModositasDTO.java
@@ -1,9 +1,13 @@
 package com.kolcsonzo.suli.sulieszkoz_kolcsonzo.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SzerepkorModositasDTO {
     @NotBlank(message = "A szerepkör neve kötelező (pl. ADMIN, ALKALMAZOTT, FELHASZNALO)")
     private String ujSzerepkor;
